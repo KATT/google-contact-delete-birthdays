7b1399eab3ea7adab2708c4c35ebadc59da71575
Alexander Johansson <alexander@n1s.se>
2025-08-03 13:27:44 +0200

    .

--- src/components/ui/theme-provider.tsx
+++ src/components/ui/theme-provider.tsx
@@ -3,6 +3,7 @@
 import { ThemeProvider } from "next-themes";
 import { useEffect, useState } from "react";
 
+// https://github.com/shadcn-ui/ui/issues/5552#issuecomment-2999470492
 const initialRender = process.env.NODE_ENV === "production";
 
 export function NextThemeProvider(
