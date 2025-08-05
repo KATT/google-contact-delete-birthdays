a6ba2d817ebefd543b8a9f5208cd3e8413cf8cea
Alexander Johansson <alexander@n1s.se>
2025-08-05 15:48:17 +0200

    .

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,5 +11,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1.0,
     },
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 }
